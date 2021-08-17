f7600e637ab98e4bd2a2b131612eafa4bd7fc702
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-08-17 12:52:15 +0700

    Create component files and css modules

--- reactjs/src/App.js
+++ reactjs/src/App.js
@@ -1,11 +1,24 @@
 import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import Header from './components/header';
+import Introduction from './components/introduction';
+import Projects from './components/projects';
+import Contact from './components/contact';
+
 import './App.css';
 
 function App() {
   return (
-    <Container>
-      <nav></nav>
-    </Container>
+    <>
+      <CssBaseline />
+      <Container>
+        <Header />
+        <Introduction />
+        <Projects />
+        <Contact />
+      </Container>
+    </>
   );
 }
 
