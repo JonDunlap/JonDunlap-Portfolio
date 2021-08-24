c1c0b68f16ed28db4f5cb08cf4ce08a87d2c0e08
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-08-24 16:21:29 +0700

    feat: Complete introduction component

--- reactjs/src/components/introduction/index.js
+++ reactjs/src/components/introduction/index.js
@@ -5,10 +5,25 @@ import * as styles from './introduction.module.css';
 
 export default function Header() {
   return (
-    <Grid item id='about' component='section'>
-      <h1>Hello World!</h1>
-      <h2 className={styles.typewriter}>My Name Is: Jonathan Dunlap</h2>
-      <h3>Web Developer</h3>
+    <Grid
+      item
+      container
+      direction='column'
+      justifyContent='flex-start'
+      alignItems='flex-start'
+      spacing={1}
+      id='about'
+      component='section'
+    >
+      <Grid item component='h1'>
+        Hello World!
+      </Grid>
+      <Grid item component='h2' className={styles.typewriter}>
+        My Name Is: Jonathan Dunlap
+      </Grid>
+      <Grid item component='h3'>
+        Web Developer
+      </Grid>
     </Grid>
   );
 }
