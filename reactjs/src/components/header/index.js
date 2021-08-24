2d8cea8b4238371e0760501052caf80f4fa20a44
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-08-24 22:48:18 +0700

    feat: add anchor tag for nav

--- reactjs/src/components/header/index.js
+++ reactjs/src/components/header/index.js
@@ -14,6 +14,7 @@ export default function Header() {
       justifyContent='flex-end'
       alignItems='center'
       className={styles.header}
+      id='top-anchor'
     >
       <Grid
         component='nav'
