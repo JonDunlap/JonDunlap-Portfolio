e4c5e557bc7f46a82d2abc18e0ce8b8f82efc0ef
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-08-24 22:10:43 +0700

    fix: adjust styles

--- reactjs/src/components/contact/index.js
+++ reactjs/src/components/contact/index.js
@@ -1,10 +1,68 @@
 import React from 'react';
+
+import Link from '@material-ui/core/Link';
 import Grid from '@material-ui/core/Grid';
+
+import * as styles from './contact.module.css';
 
 export default function Header() {
   return (
-    <Grid item id='contact' component='section'>
-      <h2>Contact</h2>
+    <Grid
+      item
+      container
+      direction='column'
+      spacing={2}
+      id='contact'
+      component='footer'
+    >
+      <Grid item component='h2'>
+        Contact
+      </Grid>
+      <Grid
+        item
+        xs
+        container
+        direction='row'
+        justifyContent='space-evenly'
+        alignItems='center'
+      >
+        <Grid item>
+          <Link
+            href='#text-buttons'
+            className={styles.link}
+            color='inherit'
+            underline='none'
+          >
+            Email
+          </Link>
+        </Grid>
+
+        <Grid item>
+          <Link
+            href='https://github.com/JonDunlap'
+            target='_blank'
+            rel='noopener noreferrer'
+            className={styles.link}
+            color='inherit'
+            underline='none'
+          >
+            GitHub
+          </Link>
+        </Grid>
+
+        <Grid item>
+          <Link
+            href='https://www.linkedin.com/in/jonathandunlap82/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className={styles.link}
+            color='inherit'
+            underline='none'
+          >
+            LinkedIn
+          </Link>
+        </Grid>
+      </Grid>
     </Grid>
   );
 }
