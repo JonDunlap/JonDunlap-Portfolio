1f38843901f189bb4f06e9bf0133eb97dda3824b
Jonathan Dunlap <jonnyd2885@gmail.com>
2021-08-29 11:53:09 +0700

    feat: create styles in css module

---
+++ reactjs/src/components/hamburgerMenu/index.js
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+
+import IconButton from '@material-ui/core/IconButton';
+import Menu from '@material-ui/core/Menu';
+import MenuIcon from '@material-ui/icons/Menu';
+
+import * as styles from './hamburgerMenu.module.css';
+
+export default function HamburgerMenu({ children }) {
+  const [anchorEl, setAnchorEl] = useState(null);
+  // methods for opening hamburger menu
+  const handleMenu = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <div>
+      <IconButton
+        aria-label='open navigation'
+        aria-controls='menu-navigation'
+        aria-haspopup='true'
+        onClick={handleMenu}
+      >
+        <MenuIcon
+          className={styles.hamburger}
+          aria-label='open drawer'
+          fontSize='medium'
+        />
+      </IconButton>
+      <Menu
+        id='menu-navigation'
+        anchorEl={anchorEl}
+        keepMounted
+        open={Boolean(anchorEl)}
+        onClose={handleClose}
+      >
+        {children}
+      </Menu>
+    </div>
+  );
+}
