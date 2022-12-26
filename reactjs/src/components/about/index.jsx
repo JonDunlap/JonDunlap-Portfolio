e9037afd669abb949c51b010529b0e22aea5ef31
Jonathan Dunlap <jonnyd2885@gmail.com>
2022-12-26 11:14:51 -0800

    Fix about me section

--- reactjs/src/components/about/index.jsx
+++ reactjs/src/components/about/index.jsx
@@ -7,24 +7,23 @@ export default function About() {
     <Grid item id='about' component='section'>
       <h2>About Me</h2>
       <p>
-        I am a military veteran with 11 years of experience in the Army, I am
-        looking to take my experience as well as my schooling and begin a career
-        as a Full Stack Java Developer. I graduated with honors and received a
-        Bachelor of Science in Web Development from Full Sail University where I
-        completed several full-stack web applications during my time as a
-        student. I love learning, and sharing my skills with others is what
-        drives me to continue to learn and teach, it is the biggest reason I
-        have gotten into being a developer. The fact that this job is always
-        growing and constantly changing, and the fact that there is just so much
-        to learn that it will always be interesting.
+        I am a military veteran with 11 years of experience in the Army, I
+        graduated with honors and received a Bachelor of Science in Web
+        Development from Full Sail University where I completed several
+        full-stack web applications during my time as a student.With additional
+        training, I have also been able to hone the skills necessary to
+        effectively use Java along with PostgreSQL, HTML, CSS, and Angular to
+        create large applications that have real-world applications in
+        businesses throughout multiple sectors. I have also learned the skills
+        and tools necessary for working in large groups, along with the planning
+        and coordination that is required to complete a large project.
       </p>
       <p>
-        With additional training, I have also been able to hone the skills
-        necessary to effectively use Java along with PostgreSQL, HTML, CSS, and
-        Angular to create large applications that have real-world applications
-        in businesses throughout multiple sectors. I have also learned the
-        skills and tools necessary for working in large groups, along with the
-        planning and coordination that is required to complete a large project.
+        I love learning, and sharing my skills with others is what drives me to
+        continue to learn and teach, it is the biggest reason I have gotten into
+        being a developer. The fact that this job is constantly growing and
+        changing, and the fact that there is just so much to learn makes it an
+        interesting career and hobby.
       </p>
       <h3>Interesting Facts About Myself:</h3>
       <ul>
