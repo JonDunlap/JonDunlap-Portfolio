68543e3a741b8b6efbb6cde3568d6fb4d6ddd6c7
Jonathan Dunlap <jonnyd2885@gmail.com>
2024-04-18 10:20:29 -0700

    updated resume link

--- reactjs/src/components/navLinks/index.jsx
+++ reactjs/src/components/navLinks/index.jsx
@@ -34,7 +34,7 @@ export default function NavLinks() {
       </Link>
 
       <Link
-        href='https://docs.google.com/document/d/e/2PACX-1vTfXnAUP-2S8TW4pS3z7ja_kDifn1wKBTFfMHI3y2NOU3AoPUF5maD-r6iL00wzsxl_5Nwl1wyGq-cI/pub'
+        href='https://docs.google.com/document/d/1RnAar8cOHkw31uadh5RUaC3WMxVhol57JvXThpRXGjE/edit?usp=sharing'
         target='_blank'
         rel='noopener noreferrer'
         className={styles.link}
