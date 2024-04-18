42dc3beba8fdaa326392426a71e2a5bc369127dd
Jonathan Dunlap <jonnyd2885@gmail.com>
2024-04-18 09:38:57 -0700

    Update index.jsx
    Updated resume link

--- reactjs/src/components/navLinks/index.jsx
+++ reactjs/src/components/navLinks/index.jsx
@@ -34,7 +34,7 @@ export default function NavLinks() {
       </Link>
 
       <Link
-        href='https://docs.google.com/document/d/1XqnmUPqJBgUv9MQXONaLWIdv1rjAHPSTc41GBXtENUk/edit?usp=sharing'
+        href='https://docs.google.com/document/d/e/2PACX-1vTfXnAUP-2S8TW4pS3z7ja_kDifn1wKBTFfMHI3y2NOU3AoPUF5maD-r6iL00wzsxl_5Nwl1wyGq-cI/pub'
         target='_blank'
         rel='noopener noreferrer'
         className={styles.link}
